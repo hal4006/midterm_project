0d8943f1e471abb684c3f01b407a1824be2e3f54
jedipawpaw <55211107+jedipawpaw@users.noreply.github.com>
2019-10-12 15:51:03 -0400

    change the sandstone background and font

--- ui.R
+++ ui.R
@@ -1,28 +1,65 @@
 library(shiny)
+library(shinythemes)
+library(plotly)
 
-cuts <- HVM$cut
+
+
+cuts <- cut_weight$cut
+sample_value <- sample(0:3000,6)
+
+cutinput <- function(x) {
+    column(6,
+              selectInput(inputId = paste0("cut_",x),
+                          label = paste0("Cut ", LETTERS[x],":"),
+                          choices = cuts, selected = cuts[x], selectize = TRUE),
+              sliderInput(inputId = paste0("quantity_",x),
+                          label = p("Pounds"),
+                          min = 0, max = 3000, value = sample_value[x], post = "lb")
+      )
+}
+
 
 shinyUI(fluidPage(
-
+    
     titlePanel("Carcass Calculator"),
-
+    theme = shinytheme("sandstone"),
+    # use custom css #### 
+    tags$head(
+      tags$link(href = "bootstrap.css", rel = "stylesheet")
+    ),
+    
     sidebarLayout(
-        sidebarPanel(
-            numericInput(inputId = "pounds_of_meat",
-                         label = "Pounds of Meat:",
-                         value = 100,
-                         min = 0,
-                         max = 10000),
-            checkboxGroupInput(inputId = "cut",
-                               label = "Cuts of Meat",
-                               cuts,
-                               selected = c("Blended Burger", "Ground Beef"))
-        ),
-
-        mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Table", tableOutput("HVM_Table")),
-                        tabPanel("Visualize the Data", plotlyOutput("HVM_Plot"), plotlyOutput("HVM_Water")))
-        )
-    )
-))
+      sidebarPanel(
+        fluidRow(cutinput(1), cutinput(2)), hr(),
+        fluidRow(cutinput(3), cutinput(4)), hr(),
+        fluidRow(cutinput(5), cutinput(6))
+      ),
+      mainPanel(
+        tabsetPanel(type = "tabs",
+                    tabPanel("Summary", 
+                             plotlyOutput("plot_cut_package"),
+                             fluidRow(
+                               column(3, 
+                                 h3("Total Heads:"),
+                                 tableOutput(outputId = "total_heads")
+                               ),
+                               column(3,offset = 1 ,
+                                 h3("Resource Use:"),
+                                 tableOutput(outputId = "resource_use")
+                               ),
+                               column(3, offset = 1,
+                                 h3("Gas Emission:"),
+                                 tableOutput(outputId = "gas_emission")
+                               )
+                             ),
+                             fluidRow(
+                               DT::dataTableOutput("table_cut_package")
+                             ),
+                             
+                             tabPanel("Comparison"),
+                             tabPanel("Overview")
+      )
+    ))
+    )))
+    
+        
